33620afed289581b2153288f47c5ecef3249418d
Andy <andy202ochoa@gmail.com>
2026-08-22 20:25:18 -0500

    Input de mensajes listo

--- src/chat.js
+++ src/chat.js
@@ -8,3 +8,17 @@ form.addEventListener("submit", (e) => {
   const mensaje = input.value.trim();
 
   if (mensaje === "") return;
+   // crear el mensaje
+  const div = document.createElement("div");
+  div.classList.add("chatUser");
+  div.textContent = mensaje;
+
+  // agregar al chat
+  chat.appendChild(div);
+
+  // limpiar input
+  input.value = "";
+
+  // scroll automático hacia abajo
+  chat.scrollTop = chat.scrollHeight;
+});
