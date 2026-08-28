8d74610c4e92f1ae6e90f48544f5edaf8beaade3
Andy <andy202ochoa@gmail.com>
2026-08-27 21:15:14 -0500

    agregue diseño a home y about estilo ciberpunck

--- src/views/about.js
+++ src/views/about.js
@@ -4,10 +4,13 @@ export default class About {
       <section class="about">
         <div class="aboutContainer">
 
-          <h1 class="aboutTitle">📖 About este proyecto</h1>
+          <h1 class="aboutTitle">📖 Acerca de este proyecto</h1>
 
           <p class="aboutText">
-            Esta es una Single Page Application (SPA) creada con JavaScript puro, usando un router sin recargar la página.
+            Mi nombre es <strong>Andy Ochoa</strong>, soy desarrollador <strong>Full Stack Junior</strong>.
+            Este proyecto fue creado para desarrollar un chat interactivo utilizando Vercel,
+            aplicando un diseño <strong>mobile-first</strong> y poniendo en práctica mis nuevos conocimientos
+            en desarrollo web.
           </p>
 
           <div class="aboutCard">
@@ -17,15 +20,41 @@ export default class About {
               <li>CSS3 (Flexbox + responsive)</li>
               <li>JavaScript (ES Modules)</li>
               <li>History API para routing</li>
+              <li>Vercel Functions</li>
             </ul>
           </div>
 
           <div class="aboutCard">
             <h2>🚀 Funcionalidades</h2>
             <ul>
-              <li>Navegación sin recarga</li>
-              <li>Chat interactivo tipo app</li>
+              <li>Navegación sin recarga (SPA)</li>
+              <li>Chat interactivo tipo aplicación</li>
               <li>Arquitectura modular (views)</li>
+              <li>Diseño responsive mobile-first</li>
+            </ul>
+          </div>
+
+          <div class="aboutCard">
+            <h2>📞 Contacto</h2>
+            <ul>
+              <li>
+                GitHub: 
+                <a href="https://github.com/andy202ochoa" target="_blank">
+                  github.com/andy202ochoa
+                </a>
+              </li>
+              <li>
+                Instagram: 
+                <a href="https://www.instagram.com/andy0xy/" target="_blank">
+                  @andy0xy
+                </a>
+              </li>
+              <li>
+                WhatsApp: 
+                <a href="https://wa.me/593961122401" target="_blank">
+                  +593 961 122 401
+                </a>
+              </li>
             </ul>
           </div>
 
